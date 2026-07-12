46624efc5d370aaf4fe7f1ad020d913e8fb6a86a
Aravind Sathesh <98135310+Aravind-Sathesh@users.noreply.github.com>
2026-07-12 23:08:13 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.63%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long ans = 1;
+        int k = min(m - 1, n - 1);
+        int steps = m + n - 2;
+        
+        for (int i=1 ; i<=k ; i++) ans = ans*(steps-k+i)/i;
+        
+        return (int)ans;
+    }
+};
