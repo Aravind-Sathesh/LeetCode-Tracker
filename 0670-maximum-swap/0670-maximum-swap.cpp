3d2fc7dbea58991fb4081c89679ca23df0596b2b
Aravind Sathesh <98135310+Aravind-Sathesh@users.noreply.github.com>
2026-07-15 23:27:49 +0530

    Time: 0 ms (100%), Space: 8 MB (64.57%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -7,11 +7,11 @@ public:
             digits.push_back(num % 10);
             num /= 10;
         }
-        
+
         reverse(digits.begin(), digits.end());
         bool swapped = false;
-        
-        for (int i = 0; i < digits.size() && !swapped; i++) {
+
+        for (int i = 0; i < digits.size(); i++) {
             int idx = i;
             for (int j = digits.size() - 1; j > i; j--) {
                 if (digits[j] > digits[idx]) idx = j;
@@ -19,7 +19,7 @@ public:
 
             if (idx != i) {
                 swap(digits[i], digits[idx]);
-                swapped = true;
+                break;
             }
         }
 
