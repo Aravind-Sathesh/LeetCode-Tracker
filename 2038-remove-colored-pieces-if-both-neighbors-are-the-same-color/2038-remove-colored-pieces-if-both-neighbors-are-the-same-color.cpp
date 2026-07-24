f2dfb4a7d8fc5418a1598027ec09fbe187cfb8a9
Aravind Sathesh <98135310+Aravind-Sathesh@users.noreply.github.com>
2026-07-24 09:14:18 +0530

    Time: 1 ms (84.87%), Space: 15.7 MB (46.55%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
     bool winnerOfGame(string colors) {
-        int n = colors.size();
-        vector <int> A, B;
-
-        if (n<=2) return false;
-        char prev = colors[0];
-        int cnt=1;
-        colors.push_back(colors[n-1]=='A' ? 'B' : 'A');
-        for (int i=1 ; i<n+1 ; i++) {
-            if (prev == colors[i]) {
-                cnt++;
-            } else {
-                if (colors[i]=='B') {
-                    A.push_back(cnt);
-                } else {
-                    B.push_back(cnt);
-                }
-                cnt=1;
+        int alice = 0, bob = 0;
+        
+        for (int i = 1; i + 1 < colors.size(); i++) {
+            if (colors[i - 1] == colors[i] && colors[i] == colors[i + 1]) {
+                if (colors[i] == 'A') alice++;
+                else bob++;
             }
-            prev = colors[i];
         }
-
-        int countA=0 , countB=0 ;
-        for (int cA : A) if (cA >=3 ) countA+=(cA-2);
-        for (int cB : B) if (cB >=3 ) countB+=(cB-2);
-
-        return (countA+countB>0 ? countA>countB : false);
-
+        
+        return alice > bob;
     }
 };
